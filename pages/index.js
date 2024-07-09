08af35ff8462b100108d61a89229841ec3c289f2
Cinthia Cavalheiro <91417907+Cinthiacs@users.noreply.github.com>
2024-07-09 21:40:39 +0000

    Adiciona Scripts`lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
-function Home(){
-    return <h1>Cinthia Cavalheiro|Análise e Desenvolvimento de Sistemas</h1>
+function Home() {
+  return <h1>Cinthia Cavalheiro|Análise e Desenvolvimento de Sistemas</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste1() {
+  console.log("identacao errada");
 }
 export default Home;
